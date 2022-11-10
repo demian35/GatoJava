80b11472ffa71c2d59a589dd7c78cb7439db8bf8
Demian <53890358+demian35@users.noreply.github.com>
2022-11-10 00:25:25 -0600

    ejecutable del juego

---
+++ JuegoGato/gato.bat
@@ -0,0 +1,22 @@
+echo BIENVENIDO %USERNAME% al juego clasico michi
+echo Juego para dos jugadores
+echo.%TIME% %DATE%
+echo Compilando Gato
+echo.
+
+::RUTA DE COMPILACION
+    ::Cambiar por la ruta donde se encuentre guardado el juego: gato\GatoJava\JuegoGato\src\GatoJava
+
+cd C:\Users\josed\OneDrive\Documentos\ProyectosdeOcio\gato\GatoJava\JuegoGato\src\GatoJava
+
+::Comando de compilacion
+javac VistaJuego.java
+
+echo Termino compilacion
+echo.
+echo EMPIEZA LA EJECUCION DEL JUEGO
+
+ java VistaJuego.java
+
+ echo. Que se diviertan , recuerden un juego limpio y sin toxixidades porfavor
+ 
